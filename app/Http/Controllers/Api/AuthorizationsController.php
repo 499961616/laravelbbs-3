9e0f78cf01f93f16da90ca6f9b081f9a4889a806
llf <llf@qq.com>
2021-07-25 15:07:32 +0800

    使用 JWT

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\Api\AuthorizationRequest;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
 use App\Models\User;
 use Illuminate\Auth\AuthenticationException;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Overtrue\LaravelSocialite\Socialite;
 
 class AuthorizationsController extends Controller
 {
+
+    //第三方登录
     public function socialStore($type,SocialAuthorizationRequest $request)
     {
         $driver = Socialite::create($type);
@@ -55,6 +58,47 @@ class AuthorizationsController extends Controller
                 }
                 break;
         }
-        return response()->json(['token' => $user->id]);
+
+        $token = auth('api')->login($user);
+        return $this->respondWithToken($token)->setStatusCode(201);
+    }
+
+    //账号密码登录
+    public function store(AuthorizationRequest $request)
+    {
+        $username = $request->username;
+
+        //过滤判断账号是否是邮箱还是手机号
+        filter_var($username,FILTER_VALIDATE_EMAIL)?
+            $credentials['email'] = $username :
+            $credentials['phone'] = $username;
+
+        $credentials['password'] = $request->password;
+
+        if (!$token = Auth::guard('api')->attempt($credentials)){
+            throw new AuthenticationException('用户名或密码错误');
+        }
+
+        return $this->respondWithToken($token)->setStatusCode(201);
+    }
+
+    public function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => Auth::guard('api')->factory()->getTTL() * 60
+        ]);
+    }
+    public function update()
+    {
+        $token = auth('api')->refresh();
+        return $this->respondWithToken($token);
+    }
+
+    public function destroy()
+    {
+        auth('api')->logout();
+        return response(null, 204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -40,6 +40,12 @@ Route::prefix('v1')
             Route::post('authorizations', 'AuthorizationsController@store')
                 ->name('authorizations.store');
 
+            // 刷新token
+            Route::put('authorizations/current', 'AuthorizationsController@update')
+                ->name('authorizations.update');
+            // 删除token
+            Route::delete('authorizations/current', 'AuthorizationsController@destroy')
+                ->name('authorizations.destroy');
 
 
         });
