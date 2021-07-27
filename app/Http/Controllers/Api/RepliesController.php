d7800742dcb13cd0e12f2ad2ec4cf1f20eb9fea5
llf <llf@qq.com>
2021-07-27 20:33:44 +0800

    回复列表

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 
+use App\Http\Queries\ReplyQuery;
 use App\Http\Requests\Api\ReplyRequest;
 use App\Http\Resources\ReplyResource;
 use App\Models\Reply;
@@ -18,5 +19,31 @@ class RepliesController extends Controller
         $reply->save();
 
         return new ReplyResource($reply);
+    }
+
+    public function destroy(Topic $topic,Reply $reply)
+    {
+
+        if ($reply->topic_id != $topic->id){
+            abort(404);
+        }
+        $this->authorize('destroy', $reply);
+        $reply->delete();
+
+        return response(null, 204);
+    }
+
+    public function index($topicId ,ReplyQuery $query)
+    {
+        $replies = $query->where('topic_id',$topicId)->paginate();
+
+        return ReplyResource::collection($replies);
+    }
+
+    public function userIndex($userId,ReplyQuery $query)
+    {
+        $replies = $query->where('user_id',$userId)->paginate();
+
+        return  ReplyResource::collection($replies);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -69,6 +69,12 @@ Route::prefix('v1')
                 //某个用户发表过的帖子
                 Route::get('users/{user}/topics', 'TopicsController@userIndex')
                     ->name('users.topics.index');
+                //话题回复列表 详情
+                Route::get('topics/{topic}/replies', 'RepliesController@index')
+                    ->name('topics.replies.index');
+                //某个用户的回复列表
+                Route::get('users/{user}/replies','RepliesController@userIndex')
+                    ->name('users.replies.index');
 
 //|------------------------------------------------|
 //|                登录后可以访问的接口
@@ -90,6 +96,9 @@ Route::prefix('v1')
                 // 发布回复
                 Route::post('topics/{topic}/replies', 'RepliesController@store')
                     ->name('topics.replies.store');
+                // 删除回复
+                Route::delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')
+                    ->name('topics.replies.destroy');
 
             });
     });
